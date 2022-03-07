229b68a299e21a9ded8e30321f38408b3ed562ef
Marzio95 <marziodellarocca14@gmail.com>
2022-03-07 17:59:14 +0100

    implemento il bonus

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 //ELEMENTI HTML
+
+const btnAddMember = document.getElementById('addMemberButton');
+let inputNome = document.getElementById('name');
+let inputRuolo = document.getElementById('role');
+let inputImg = document.getElementById('image');
+
 // let cartaTeamMate = document.querySelector('.team-card');
 // console.log();
 // let nomeTeamMate = document.querySelector('.team-card .card-text h3');
@@ -107,3 +113,29 @@ function addTeamMates(arrObject) {
 }
 
 addTeamMates(arrayObject);
+
+
+//FUNZIONE APPENDI NEW MATES
+
+function newTeamMates () {
+    let carta = document.createElement('div');
+    carta.classList.add('team-card');
+    let immagine = document.createElement('div');
+    immagine.classList.add('card-image');
+    carta.append(immagine);
+    let img = document.createElement('img');
+    immagine.append(img);
+    let testoCarte = document.createElement('div');
+    testoCarte.classList.add('card-text');
+    let nomeInLabel = document.createElement('h3');
+    let ruoloInLabel = document.createElement('p');
+    testoCarte.append(nomeInLabel);
+    testoCarte.append(ruoloInLabel);
+    carta.append(testoCarte);
+    img.src = inputImg.value;
+    nomeInLabel.innerHTML = inputNome.value;
+    ruoloInLabel.innerHTML = inputRuolo.value;
+    contenitoreCarte.append(carta);
+}
+
+btnAddMember.addEventListener('click', newTeamMates);
